b0bc87aa6eb0f83e2124f82c1b3f322863149a1f
Nogs0 <joaoguinogueira04@gmail.com>
2022-11-21 16:11:01 -0300

    CMake On

---
+++ KJM/CMakeFiles/Head.h
@@ -0,0 +1,46 @@
+#ifndef HEAD_H_INCLUDED
+#define HEAD_H_INCLUDED
+
+#include <stdlib.h>
+#include <stdio.h>
+#include <string.h>
+#include <time.h>
+#include <ctype.h>
+
+typedef struct{
+    char Descricao[200];
+    char CodigoB[200];
+    char Fornecedor[50];
+    int Dia;
+    int Mes;
+    int Ano;
+}Produto;
+typedef struct no{
+    struct no *ant;
+    Produto p;
+    struct no *prox;
+}No;
+
+typedef struct{
+    No *inicio;
+    No *fim;
+}Lista;
+
+void clearscr(void);
+void CriaLista(Lista *);
+int  InsereFinal(Lista *, Produto);
+int  InsereOrdenado(Lista *, Produto);
+int  Tamanho(Lista);
+void Mostra(Lista);
+void AdicionandoProduto(Lista *, Lista *);
+int  Remover(Lista*, Produto);
+void RemovendoProduto(Lista*);
+int  DataValida(int, int, int);
+struct tm DataAtual();
+void ConferirValidade(Lista);
+void Salvar(Lista);
+void SalvandoProdutos(Lista*);
+int ConfereEmp(char*);
+int ConfereCod(char*, Produto*, Lista *);
+int InserirMemoria(char *Empresa, Lista *l);
+#endif // HEAD_H_INCLUDED
